912e2a17da189076656d0c0f4b0c88ce96ca371a
jNeves tuffo 67 <jaomarcio79@gmail.com>
2026-03-30 22:02:22 -0300

    curso

---
+++ Logica_de_Programação/30.03/aula_02.js
@@ -0,0 +1,15 @@
+// var (palavra reservada)
+// [NOME DA VARIÉVEL] 
+// = (OPERADOR DE ATRIBUIÇÃO) 
+// Após o sinal de igual, é o valor atribuido á variável
+var numero = 10;
+
+//Tipo de declaração inserido nas últimas versões
+let numero2 = 4.56;
+
+// Constantes não podem modificar seu valor após atribuição
+const PI = 3.14;
+
+// Chamando a varável numero
+// Para imprimir seu valor contido
+console.log(numero)
